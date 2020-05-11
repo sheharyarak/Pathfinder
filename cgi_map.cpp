e2919824f7d02f9ec75c0c9e468302dbc49b83e2
Sheharyar Alam Khan <31715063+sheharyarak@users.noreply.github.com>
2020-05-11 06:46:15 -0700

    got the map cgi to work

--- cgi_map.cpp
+++ cgi_map.cpp
@@ -9,17 +9,23 @@ int		main()
 	// std::cerr << "here" << std::endl;
 	std::ofstream cart("cart.txt");
 	// std::cerr << "here" << std::endl;
-	for(auto iter = ui.queries().begin(); iter != ui.queries().end(); iter++)
+	std::map<std::string, std::string>::iterator start = ui.queries().begin();
+	std::map<std::string, std::string>::iterator end = ui.queries().end();
+
+	for(auto iter = start; iter != end; iter++)
 	{
 		// std::cerr << "here" << std::endl;
 		if(iter->first != "Inventory")
 			cart << iter->second << std::endl;
+		else
+			break;
+		
 		// std::cout << iter->first << " --> " << iter->second << std::endl;
 	}
 	// std::string inv = ui.param("Inventory");
-	std::cout << "here" << std::endl;
+	// std::cout << "here" << std::endl;
 	std::cout	<<	"<body>" << std::endl;
-	std::cout	<<	"<p>" << ui.get_query_string() << "</p>";
+	// std::cout	<<	"<p>" << ui.get_query_string() << "</p>";
 	std::cout	<<	"	<form action=\"http://localhost/cgi-bin/Pathfinder/Pathfinder.cgi\">" << std::endl;
 	std::cout	<<	"		<input type=\"hidden\" name=\"Inventory\" value=\"" << ui.param("Inventory") << "\">" << std::endl;
 	std::cout	<<	"		<input type=\"hidden\" name=\"Cart\" value=\"" << "cart.txt" << "\">"  << std::endl;
